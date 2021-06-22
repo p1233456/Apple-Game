8f52500dd712902470160de4621321576ec9ed85
ginyang2 <59835095+p1233456@users.noreply.github.com>
2021-06-22 17:52:01 +0900

    Add DragControl

---
+++ Assets/Scripts/DragControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragControl : MonoBehaviour
+{
+    private Vector2 MousePosition
+    {
+        get
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private bool isDragging { get { return instatiatedDragSquare != null; } }
+    [SerializeField]
+    private Vector2 dragStart;
+    [SerializeField]
+    private GameObject dragSquare;
+    private GameObject instatiatedDragSquare;
+
+    private void Start()
+    {
+        instatiatedDragSquare = null;
+    }
+
+    private void Update()
+    {
+        DragInput();
+        if (isDragging)
+        {
+            SetDragSquare(instatiatedDragSquare, dragStart, MousePosition);
+        }
+    }
+
+    private void SetDragSquare(GameObject square, Vector2 dragStartPoint, Vector2 currentMousePosition)
+    {
+        square.transform.position = new Vector2((currentMousePosition.x - dragStartPoint.x) / 2 + dragStartPoint.x, (currentMousePosition.y - dragStartPoint.y) / 2 + dragStartPoint.y);
+        square.transform.localScale = new Vector2(currentMousePosition.x - dragStartPoint.x, currentMousePosition.y - dragStartPoint.y);
+    }
+
+    private void DragStart()
+    {
+        dragStart = MousePosition;
+        instatiatedDragSquare = Instantiate(dragSquare,dragStart,Quaternion.identity);
+    }
+
+    private void DragEnd()
+    {
+        Destroy(instatiatedDragSquare);
+        instatiatedDragSquare = null;
+    }
+
+    private void DragInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+            DragStart();
+        if (Input.GetMouseButtonUp(0))
+            DragEnd();
+    }
+
+}
