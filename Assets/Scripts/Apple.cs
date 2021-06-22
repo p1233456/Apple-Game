81f4bbf394c00afb2c3271c181688eb2122312c8
ginyang2 <59835095+p1233456@users.noreply.github.com>
2021-06-22 18:08:12 +0900

    Add Apple

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Apple : MonoBehaviour
+{
+    private int uniqueNum;
+    public int UniquNum { get { return uniqueNum; } }
+    [SerializeField]
+    Text text;
+
+    private void Update()
+    {
+        text.text = uniqueNum.ToString();
+    }
+
+    public void SetUniqueNum()
+    {
+        uniqueNum = Random.Range(1, 10);
+    }
+}
